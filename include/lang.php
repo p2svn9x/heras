7541cf54f7681b2463c9ae072489368cd21eb041
p2svn9x <37497642+p2svn9x@users.noreply.github.com>
2018-09-04 00:57:58 +0700

    Update lang.php

--- include/lang.php
+++ include/lang.php
@@ -1,12 +1,12 @@
 <?php
-$login_path="login.php";
-$home_path="index.php";
-$card_path="card.php";
-$bin_path="bin.php";
-$dump_path="dump.php";
-$dumpformat_path="dump_format.php";
-$filter_path="filter.php";
-$hack_path="hack.php";
+$login_path="103.237.145.79/login.php";
+$home_path="103.237.145.79/index.php";
+$card_path="103.237.145.79/card.php";
+$bin_path="103.237.145.79/bin.php";
+$dump_path="103.237.145.79/dump.php";
+$dumpformat_path="103.237.145.79/dump_format.php";
+$filter_path="103.237.145.79/filter.php";
+$hack_path="103.237.145.79/hack.php";
 
 
 
