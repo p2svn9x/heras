7ef1b9379bcc8a614781736f564f5a3491fbdf88
p2svn9x <37497642+p2svn9x@users.noreply.github.com>
2018-09-04 01:12:13 +0700

    Update login.php

--- login.php
+++ login.php
@@ -62,6 +62,7 @@ if (isset($_SESSION['user'])){ header("Location: $home_patch");} ?>
                         <span class="help-block">Password</span>
                       </div>
                     </div>
+          <button type="submit" class="btn btn-sm btn-primary"><i class="fa fa-dot-circle-o"></i> Login</button>
                   </form>
 
               </div>
